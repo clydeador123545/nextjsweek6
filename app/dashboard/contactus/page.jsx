5594ad5879f64ef6bd4b52e86d468f73da017971
clydetims <clydeador39@gmail.com>
2025-06-16 14:17:50 +0800

    first commit

---
+++ app/dashboard/contactus/page.jsx
@@ -0,0 +1,8 @@
+
+
+
+export default function Page(){
+    return(
+        <div>Contact Us</div>
+    );
+}
---
+++ app/dashboard/invoices/page.jsx
@@ -0,0 +1,7 @@
+import '../../ui/global.css'
+
+export default function Page() {
+    return(
+        <div className="">Invoices</div>
+    );
+}
